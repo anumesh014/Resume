204f0dc835c1e692c8e1ecce7ac4dfb303ab1e51
Anumesh <124165628+anumesh014@users.noreply.github.com>
2024-08-12 17:07:53 +0545

    Add files via upload

---
+++ cc.js
@@ -0,0 +1,14 @@
+var main = document.querySelector("#main")
+var crsr = document.querySelector(".cursor")
+
+main.addEventListener("mousemove",function(dets){
+    crsr.style.left = dets.x+"px"
+    crsr.style.top = dets.y+"px"
+})
+
+// main.addEventListener("mousemove",function(dets){
+//     console.log(dets)
+//     console.log(dets.x)
+//     console.log(dets.y)
+// I am getting details of mouse move through this
+// })
